5f4b825f6d6712d83637cbc5ea2aa963abc94e5b
Gigihrahman <gigihrahmanditta@gmail.com>
2025-03-12 15:11:04 +0700

    feat: behaviour smooth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className="antialiased bg-[#EAE8E1] overflow-y-auto min-h-screen px-3 sm:px-4 md:px-6">
         <div className="container mx-auto bg-white mt-6 justify-center rounded-t-xl block w-[95%] sm:w-[94%] md:w-[92%] lg:w-[90%]">
           <Navbar />
